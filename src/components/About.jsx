f2d5ff37c7bdaa65559e9f6d35e42aef3b827d68
samueljsong <semuall0108@gmail.com>
2024-08-23 14:54:34 -0700

    working on about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,6 +18,15 @@ gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
 export const About = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 575.98);
+
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            console.log("window is resized");
+            setIsMobile(window.innerWidth < 575.98);
+        });
+    });
+
     useGSAP(() => {
         const t1 = gsap.timeline();
 
@@ -39,35 +48,85 @@ export const About = () => {
     return (
         <section className="about">
             <div className="about-container">
-                <div className="about-info-container">
-                    <div className="about-text-container">
-                        <span className="heading2 about-text">Hey</span>
-                        <span className="heading2 about-text">I'm</span>
-                        <span className="heading2 about-text">
-                            <span className="about-name">Samuel</span>,
-                        </span>
-                        <span className="heading2 about-text">a</span>
-                        <span className="heading2 about-text">software</span>
+                {isMobile ? (
+                    <div className="about-info-container">
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">Hey</span>
+                            <span className="heading2 about-text">I'm</span>
+                            <span className="heading2 about-text">
+                                <span className="about-name">Samuel</span>,
+                            </span>
+                        </div>
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">a</span>
+                            <span className="heading2 about-text">
+                                software
+                            </span>
+                            <span className="heading2 about-text">
+                                developer
+                            </span>
+                        </div>
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">
+                                currently
+                            </span>
+                            <span className="heading2 about-text">looking</span>
+                            <span className="heading2 about-text">for</span>
+                        </div>
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">new</span>
+                            <span className="heading2 about-text">
+                                opportunities
+                            </span>
+                            <span className="heading2 about-text">to</span>
+                        </div>
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">expand</span>
+                            <span className="heading2 about-text">my</span>
+                            <span className="heading2 about-text">
+                                skillset.
+                            </span>
+                        </div>
                     </div>
-                    <div className="about-text-container">
-                        <span className="heading2 about-text">developer</span>
-                        <span className="heading2 about-text">currently</span>
-                        <span className="heading2 about-text">looking</span>
+                ) : (
+                    <div className="about-info-container">
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">Hey</span>
+                            <span className="heading2 about-text">I'm</span>
+                            <span className="heading2 about-text">
+                                <span className="about-name">Samuel</span>,
+                            </span>
+                            <span className="heading2 about-text">a</span>
+                            <span className="heading2 about-text">
+                                software
+                            </span>
+                        </div>
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">
+                                developer
+                            </span>
+                            <span className="heading2 about-text">
+                                currently
+                            </span>
+                            <span className="heading2 about-text">looking</span>
+                        </div>
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">for</span>
+                            <span className="heading2 about-text">new</span>
+                            <span className="heading2 about-text">
+                                opportunities
+                            </span>
+                        </div>
+                        <div className="about-text-container">
+                            <span className="heading2 about-text">to</span>
+                            <span className="heading2 about-text">expand</span>
+                            <span className="heading2 about-text">my</span>
+                            <span className="heading2 about-text">
+                                skillset.
+                            </span>
+                        </div>
                     </div>
-                    <div className="about-text-container">
-                        <span className="heading2 about-text">for</span>
-                        <span className="heading2 about-text">new</span>
-                        <span className="heading2 about-text">
-                            opportunities
-                        </span>
-                    </div>
-                    <div className="about-text-container">
-                        <span className="heading2 about-text">to</span>
-                        <span className="heading2 about-text">expand</span>
-                        <span className="heading2 about-text">my</span>
-                        <span className="heading2 about-text">skillset.</span>
-                    </div>
-                </div>
+                )}
             </div>
         </section>
     );
